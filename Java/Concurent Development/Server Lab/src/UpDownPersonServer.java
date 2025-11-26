fa0ee084fe4861b2ea677e1329095788f8376be6
srgwx <runnershadowx@gmail.com>
2025-11-26 16:20:20 +0000

    Concurrent Development <Server lab> ( UpDownPersonServer.java add comments to the code)

--- Java/Concurent Development/Server Lab/src/UpDownPersonServer.java
+++ Java/Concurent Development/Server Lab/src/UpDownPersonServer.java
@@ -1,26 +1,36 @@
 import java.io.*;
 import java.net.*;
 
+// Student name: Stanislav Kril
+// Student number: 3133810
+
+
+// Class simulate networked server that accepts incoming connections on a specified port.
 public class UpDownPersonServer {
-    final static int portPerson = 1235; // any number > 1024
+
+    // dedicated port for communication with clients
+    final static int portPerson = 1235; // any number greater than 1024
 
     public static void main(String[] args) {
-
         System.out.println("Server running...");
+
+        // create a new data set to store and manage person objects
         Data data = new Data();
 
         try {
+            // create a server socket on the designated port
             ServerSocket serverSocket = new ServerSocket(portPerson);
 
+            // enter infinite loop to continuously accept incoming connections
             while (true) {
-
-                // wait for a service request on port portSqrt
+                // wait for a service request from a client on the specified port
                 Socket socket = serverSocket.accept();
 
-                // start thread to service request
+                // start a new thread to handle the incoming request and process it
                 new PersonUpDown(socket, data).start();
             }
         } catch (IOException e) {
+            // print any error messages that occur during server initialization or operation
             e.printStackTrace();
         }
     }
