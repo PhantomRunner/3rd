230b5f2d9101d820b72f17c7586215c5661c9871
srgwx <runnershadowx@gmail.com>
2025-11-28 16:21:25 +0000

    Concurrent Development (Assignment 3) < Server.java. Implement Server class to simulate working server >

--- Java/Concurent Development/Assignment 3/Assignment 3/src/Server.java
+++ Java/Concurent Development/Assignment 3/Assignment 3/src/Server.java
@@ -1,23 +1,28 @@
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 // Class who simulates the server
 public class Server {
-
-    final static int PORT = 1234;
+     static final int PORT = 1234;
+     static CarDatabase carDatabase;;
 
     public static void main(String[] args) {
 
         System.out.println("Server starting...");
 
-        Data data = new Data();
-
         try {
             ServerSocket serverSocket = new ServerSocket(PORT);
+            carDatabase = new CarDatabase();
+            ExecutorService threadPool = Executors.newFixedThreadPool(50);
 
             while (true) {
-                Socket socket = serverSocket.accept();
-                new CarUpDown(socket, data).start();
+                Socket clientSocket = serverSocket.accept();
+                ClientHandler handler = new ClientHandler(clientSocket, carDatabase);
+                threadPool.submit(handler);
+
+
             }
 
         } catch (Exception e) {
