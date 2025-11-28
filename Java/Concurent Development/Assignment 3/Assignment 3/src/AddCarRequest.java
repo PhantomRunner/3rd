974f5d193a394299a1bbfbca5d4125ed171a614b
srgwx <runnershadowx@gmail.com>
2025-11-28 17:59:03 +0000

    [Concurrent Development][Assignment 3] Add Comments for AddCarRequest.java

--- Java/Concurent Development/Assignment 3/Assignment 3/src/AddCarRequest.java
+++ Java/Concurent Development/Assignment 3/Assignment 3/src/AddCarRequest.java
@@ -1,6 +1,9 @@
 import java.io.Serializable;
 
+// Request to add a new car to the database
 public class AddCarRequest implements Serializable {
+
+    // Car object which will be added
     private Car car;
 
     public AddCarRequest(Car car) {
