15d0d4d5ea8971f7111cf71b0e130a64bdbabf48
srgwx <runnershadowx@gmail.com>
2025-11-28 18:02:52 +0000

    [Concurrent Development][Assignment 3] Add Comments for TotalSellValueRequest.java

--- Java/Concurent Development/Assignment 3/Assignment 3/src/TotalSellValueRequest.java
+++ Java/Concurent Development/Assignment 3/Assignment 3/src/TotalSellValueRequest.java
@@ -1,5 +1,6 @@
 import java.io.Serializable;
 
+// Request to calculate the total value of all sold cars
 public class TotalSellValueRequest implements Serializable {
 
     public TotalSellValueRequest() {}
