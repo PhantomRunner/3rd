235b9b352f836216d65fa22a1cdb71f07645876f
srgwx <runnershadowx@gmail.com>
2025-11-28 18:06:38 +0000

    [Concurrent Development][Assignment 3] Add Comments for CarDatabase.java

--- Java/Concurent Development/Assignment 3/Assignment 3/src/CarDatabase.java
+++ Java/Concurent Development/Assignment 3/Assignment 3/src/CarDatabase.java
@@ -1,33 +1,37 @@
 import java.util.ArrayList;
 
-
-// Class to store data on server
+// Stores and manages all cars on the server
 public class CarDatabase {
 
+    // List of all cars
     private ArrayList<Car> data = new ArrayList<Car>();
 
-
+    // Adds a new car if its registration does not already exist in data set
     public synchronized boolean addCar(Car car) {
 
         for (Car c : data) {
-            if ((c.getRegistration().equals(car.getRegistration()))) {
+            if (c.getRegistration().equals(car.getRegistration())) {
+                // Car already exists so reject it
                 return false;
             }
         }
+        // Add car and return postive flag
         data.add(car);
         return true;
     }
 
+    // Marks a car as sold based on its registration
     public synchronized boolean sellCar(String registration) {
         for (Car car : data) {
             if (car.getRegistration().equals(registration) && car.forSale()) {
                 car.setForSale(false);
-                return true;
+                return true; // Car sold
             }
         }
-        return false;
+        return false; // Car not found or already sold
     }
 
+    // Returns all cars currently for sale
     public synchronized ArrayList<Car> listCarsForSale() {
         ArrayList<Car> saleList = new ArrayList<Car>();
         for (Car car : data) {
@@ -38,6 +42,7 @@ public class CarDatabase {
         return saleList;
     }
 
+    // Returns all cars matching a specific make
     public synchronized ArrayList<Car> listCarsByMake(String make) {
         ArrayList<Car> makeList = new ArrayList<Car>();
         for (Car car : data) {
@@ -48,11 +53,12 @@ public class CarDatabase {
         return makeList;
     }
 
+    // Calculates total value of all cars that have been sold
     public synchronized double totalSellValue() {
         double totalSellValue = 0;
         for (Car car : data) {
             if (!car.forSale()) {
-                totalSellValue = totalSellValue + car.getPrice();
+                totalSellValue += car.getPrice();
             }
         }
         return totalSellValue;
