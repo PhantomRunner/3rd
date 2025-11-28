299ed475f32b3fbbc26c8a0541eb47e071cec624
srgwx <runnershadowx@gmail.com>
2025-11-28 18:02:37 +0000

    [Concurrent Development][Assignment 3] Add Comments for SellCarRequest.java

--- Java/Concurent Development/Assignment 3/Assignment 3/src/SellCarRequest.java
+++ Java/Concurent Development/Assignment 3/Assignment 3/src/SellCarRequest.java
@@ -1,6 +1,9 @@
 import java.io.Serializable;
 
+// Request to mark a car as sold
 public class SellCarRequest implements Serializable {
+
+    // Registration of the car to sell
     private String registration;
 
     public SellCarRequest(String registration) {
