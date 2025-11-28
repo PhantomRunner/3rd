a08429193c9a0806867d058e98af9fd45fd1d97b
srgwx <runnershadowx@gmail.com>
2025-11-28 18:00:22 +0000

    [Concurrent Development][Assignment 3] Add Comments for ListCarsByMakeRequest.java

--- Java/Concurent Development/Assignment 3/Assignment 3/src/ListCarsByMakeRequest.java
+++ Java/Concurent Development/Assignment 3/Assignment 3/src/ListCarsByMakeRequest.java
@@ -1,7 +1,9 @@
 import java.io.Serializable;
 
+// Request for listing cars filtered by a specific make
 public class ListCarsByMakeRequest implements Serializable {
 
+    // Car make filter
     private String make;
 
     public ListCarsByMakeRequest(String make) {
